08747c84a154dbfe2c99cb522056e75719184615
Matt Farmer <mrfarmer777@gmail.com>
2018-05-31 03:13:18 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
 
+  def initialize(name)
+    @name=name
+    @balance=1000
+    @status='open'
+  end
+
+  def deposit(amt)
+    @balance+=amt
+  end
+
+  def display_balance
+    @balance  #possibly need puts here
+  end
+
+  def valid?
+    if @status=='open' && @balance>0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status="closed"
+    @balance=0
+  end
 end
