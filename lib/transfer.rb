9e7b4af3d8b565580783a35a94ebc6753f58f0dc
Matt Farmer <mrfarmer777@gmail.com>
2018-05-31 03:39:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,9 @@ class Transfer
       @receiver.balance+=@amount
       @status="complete"
     else
-      "Transaction rejected. Please check your account balance."
       @status='rejected'
+      "Transaction rejected. Please check your account balance."
+      
     end
   end
 
