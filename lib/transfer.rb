6a05a4a66b93fa28e5aef90303a67a09d85a95aa
Matt Farmer <mrfarmer777@gmail.com>
2018-05-31 03:21:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,5 +14,15 @@ class Transfer
     sender.valid? && receiver.valid? ? true : false
   end
 
+  def execute_transaction
+    if self.valid?
+      @sender.balance-=@amount
+      @receiver.balance+=@amount
+      @status="executed"
+    else
+      "Your account is not valid"
+    end
+  end
   
+
 end
